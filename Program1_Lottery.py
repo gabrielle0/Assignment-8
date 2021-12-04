cfc6c28998daa735ca271d831002a79db89648ec
unknown <gabrielleapangilinan@iskolarngbayan.pup.edu.ph>
2021-12-04 19:37:00 +0800

    added get number function for getting user input and generating random numbers

--- Program1_Lottery.py
+++ Program1_Lottery.py
@@ -6,3 +6,41 @@
 #Step 5 Display ”Try again y/n” after each game
 # If the user enter “y” the user will play again
 # if “n” the program will exit
+
+
+import random
+
+
+def trial ():
+    user_Input = "yes"
+    while user_Input [0] == "y":
+        first = int (input ("Enter first number, from 0-9:  "))
+        second = int (input ("Enter second number, 0-9:  "))
+        third = int (input("Enter third number: "))
+
+        gen1 = random.randint (0,9)
+        gen2 = random.randint (0,9)
+        gen3 = random.randint (0,9)
+
+        score = 0
+
+        if first == gen1 or gen2 or gen3:
+            score += 1
+        if second == gen1 or gen2 or gen3:
+            score += 1
+        if third == gen1 or gen2 or gen3:
+            score += 1
+        
+        if score == 3:
+            print ("Winner!")
+        else:
+            print ("You lose")
+        
+        user_Input = input ("Try again? y/n")
+    else:
+        exit ()
+
+
+trial()
+
+
