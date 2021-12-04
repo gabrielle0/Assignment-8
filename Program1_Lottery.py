1d54bcc9d09c73a54943d0a00135966248c9dcbf
unknown <gabrielleapangilinan@iskolarngbayan.pup.edu.ph>
2021-12-04 14:35:32 +0800

    added code for getting user input

---
+++ Program1_Lottery.py
@@ -0,0 +1,8 @@
+#Steps:
+#Step 1: Create a program that ask 3 numbers (0-9) from the user.
+#Step 2: Generate 3 random winning numbers (0-9)
+#Step 3: Display “Winner” if all 3 input numbers matched the generated numbers
+#Steo 4:  Display ”You loss” if not
+#Step 5 Display ”Try again y/n” after each game
+# If the user enter “y” the user will play again
+# if “n” the program will exit
